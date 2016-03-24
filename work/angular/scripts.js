a90cee64023861d226928d6046c13774edf3e787
Jamie Pettman <james.pettman@waitrose.co.uk>
2016-03-24 16:26:47 +0000

    fix
    Was adding currentUser to their own friends

--- work/angular/scripts.js
+++ work/angular/scripts.js
@@ -133,7 +133,7 @@ mApp.controller('profileController', ['$scope', '$firebaseObject', '$firebaseArr
     $scope.addFriend = function(){
         debugger;
         currentProfileFriendsID.$add($scope.currentUser.id);
-        $scope.currentUserContactsArray.$add($scope.currentUser.id);
+        $scope.currentUserContactsArray.$add($scope.currentProfilePublic.id);
     };
     $scope.removeFriend = function(){
 
