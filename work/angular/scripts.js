31943442d3715ff5e2b9fd82617d7209ee1df420
Jamie Pettman <james.pettman@waitrose.co.uk>
2016-03-31 11:57:09 +0100

    birthdate

--- work/angular/scripts.js
+++ work/angular/scripts.js
@@ -165,6 +165,8 @@ mApp.controller('profileController', ['$scope', '$firebaseObject', '$firebaseArr
         $('.button-collapse').sideNav('hide');
         console.log($scope.addUserName);
         console.log($scope.addUserDOB);
+        var birthdate = new Date($scope.date).toLocaleDateString('en-GB');
+        console.log($scope.addUserDOB);
         console.log($scope.addUserPM);
         $scope.users.$add({
             name: 'Jess'
