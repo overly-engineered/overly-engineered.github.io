770951cc2848de6d4fb57ff3d9d80d82fccdb747
Jamie Pettman <james.pettman@waitrose.co.uk>
2017-01-05 12:29:31 +0000

    removed some projects to their own repositories

--- work/ember-app/app/models/comment.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  message: DS.attr('string'),
-  date: DS.attr('string'),
-  name: DS.attr('string'),
-  post_id: DS.attr('string')
-});
--- work/react/app/src/js/main.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-var React = require('react'),
-    Boards = require('./components/Boards');
-
-React.renderComponent(
-  <Boards />,
-  document.getElementById('app')
-);
