3635912bace68570704a7e38948d793d469e23b1
Jamie Pettman <james.pettman@ripjar.com>
2020-02-27 14:11:29 +0000

    Console update

--- app/js/console.js
+++ app/js/console.js
@@ -1,22 +1,11 @@
-(function () {
-    console.log("%c Hey, my name is Jamie.", "color: rebeccapurple; font-size:14px; line-height: 2;");
-    console.log("%c I make web apps and websites.", "color: rebeccapurple; font-size:14px; line-height: 2;");
-    console.log("%c Did you know jamie is an object on window? Sure you can find out some things if you wanted to.", "color: rebeccapurple; font-size:14px;");
-    var goTo = function (o) {
-        window.open(o, "_blank")
-    };
-    window.jamie = {
-        github: function () {
-            goTo("https://github.com/overly-engineered")
-        },
-        codePen: function () {
-            goTo("https://codepen.io/overlyenginnered/")
-        },
-        email: function () {
-            goTo("jwrpettman@gmail.com")
-        },
-        twitter: function () {
-            goTo("https://twitter.com/PettmanJamie")
-        }
-    }
+(function() {
+  console.log("Hey, my name is Jamie.");
+  console.log("I make web apps and websites.");
+  window.jamie = {
+    github: "https://github.com/overly-engineered",
+    codePen: "https://codepen.io/overlyenginnered/",
+    email: "jwrpettman@gmail.com",
+    twitter: "https://twitter.com/PettmanJamie"
+  };
+  console.log(window.jamie);
 })();
